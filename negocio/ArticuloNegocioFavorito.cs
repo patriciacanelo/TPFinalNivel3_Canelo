3ae995b0c84db2e173e80cd6b49874fd6143b2a2
Patricia Canelo <57050803+patriciacanelo@users.noreply.github.com>
2023-03-21 17:33:56 -0300

    Add files via upload

---
+++ negocio/ArticuloNegocioFavorito.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class ArticuloNegocioFavorito
+    {
+        public List<Articulo> listarFavoritoConSP(ArticuloFavorito nuevo)
+        {
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("select A.Id, A.Nombre, A.Descripcion, A.Precio from FAVORITOS, ARTICULOS A where IdUser = @IdUser and IdArticulo = A.Id ");
+                datos.setearParametro("@IdUser", nuevo.IdUser);
+                datos.setearParametro("@IdArticulo", nuevo.IdArticulo);
+                datos.ejecutarLectura();
+                
+                while (datos.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    aux.Precio = (decimal)datos.Lector["Precio"];
+
+                    lista.Add(aux);
+
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void DeleteFavorito(ArticuloFavorito nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Delete from FAVORITOS where IdUser = @IdUser and IdArticulo = @IdArticulo");
+                datos.setearParametro("@IdUser", nuevo.IdUser);
+                datos.setearParametro("@IdArticulo", nuevo.IdArticulo);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void insertarFavoritoConSP(ArticuloFavorito nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearProcedimiento("insertarFavorito");
+                datos.setearParametro("@idUser", nuevo.IdUser);
+                datos.setearParametro("@idArticulo", nuevo.IdArticulo);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        
+    }
+}
---
+++ negocio/EmailService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace negocio
+{
+    public class EmailService
+    {
+        private MailMessage email;
+        private SmtpClient server;
+
+        public EmailService()
+        {
+            server = new SmtpClient();
+            server.Credentials = new NetworkCredential("8897c4f7d8c453", "de51441bc03f38");
+            server.EnableSsl = true;
+            server.Port = 2525;
+            server.Host = "smtp.mailtrap.io";
+        }
+
+        public void armarCorreo(string emailDestino, string asunto, string cuerpo)
+        {
+            email = new MailMessage();
+            email.From = new MailAddress("noresponder@softechtarticulos.com");
+            email.To.Add(emailDestino);
+            email.Subject = asunto;
+            email.IsBodyHtml = true;
+            email.Body = cuerpo;
+        }
+
+        public void enviarEmail()
+        {
+            try
+            {
+                server.Send(email);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
